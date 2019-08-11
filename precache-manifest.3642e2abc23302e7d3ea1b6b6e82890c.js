b52441e49b8b1e3f698cc8cc7ef568573129402b
Julian Ibarra <yeissonibarra@gmail.com>
2019-08-11 21:37:22 +0200

    Updates

---
+++ precache-manifest.3642e2abc23302e7d3ea1b6b6e82890c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771056533a83e60276d63ad339ec1946",
+    "url": "/calendar-colors/index.html"
+  },
+  {
+    "revision": "b7ac0dd362dffae7a2d4",
+    "url": "/calendar-colors/static/css/main.62dcaf35.chunk.css"
+  },
+  {
+    "revision": "48cad16eb59fc2a7d942",
+    "url": "/calendar-colors/static/js/2.2cf407b3.chunk.js"
+  },
+  {
+    "revision": "b7ac0dd362dffae7a2d4",
+    "url": "/calendar-colors/static/js/main.7086c18f.chunk.js"
+  },
+  {
+    "revision": "6d925df846795954e28e",
+    "url": "/calendar-colors/static/js/runtime~main.288dbf93.js"
+  }
+]);
